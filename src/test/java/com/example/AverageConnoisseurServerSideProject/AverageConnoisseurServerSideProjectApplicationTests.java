a17fd33dddaf5681d38118f0a0caaabbc3cc4bac
Luke Fowles <lukefowles@Lukes-MacBook-Air.local>
2021-11-11 12:27:44 +0000

    added set-up files for spring boot application

---
+++ src/test/java/com/example/AverageConnoisseurServerSideProject/AverageConnoisseurServerSideProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AverageConnoisseurServerSideProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AverageConnoisseurServerSideProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
