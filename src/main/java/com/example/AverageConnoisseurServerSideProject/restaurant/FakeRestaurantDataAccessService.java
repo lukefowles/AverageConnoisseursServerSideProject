9c3a9f7afe4f7179cf579cfeb2c1eeb4aa332962
Luke Fowles <lukefowles@Lukes-MacBook-Air.local>
2021-11-12 11:45:17 +0000

    bug fix: reposotories has the same name, prevent http requests

--- src/main/java/com/example/AverageConnoisseurServerSideProject/restaurant/FakeRestaurantDataAccessService.java
+++ src/main/java/com/example/AverageConnoisseurServerSideProject/restaurant/FakeRestaurantDataAccessService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-@Repository("Fake")
+@Repository("FakeRestaurantData")
 public class FakeRestaurantDataAccessService implements RestaurantDAO {
     private List<Restaurant> db = new ArrayList<>();
 
