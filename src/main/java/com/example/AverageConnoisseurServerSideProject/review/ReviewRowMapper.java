894aa9b824343945a44d7ba26d726091677c0756
Jessica Morse <jess0morse99@gmail.com>
2021-11-14 17:12:36 +0000

    added ReviewDataAccessService and ReviewRowMapper classes

---
+++ src/main/java/com/example/AverageConnoisseurServerSideProject/review/ReviewRowMapper.java
@@ -0,0 +1,20 @@
+package com.example.AverageConnoisseurServerSideProject.review;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ReviewRowMapper implements RowMapper<Review> {
+
+    @Override
+    public Review mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Review review = new Review (
+                rs.getInt("id"),
+        rs.getInt("restaurant_ID"),
+        rs.getInt("customer_ID"),
+        rs.getInt("rating"),
+        rs.getString("comment")
+        );
+        return review;    }
+}
